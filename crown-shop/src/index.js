3dccdce6e7e1c33ff0ca6d9daa57d32f5ee2a391
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-13 11:40:46 +0500

    converting states into redux, local storage

--- crown-shop/src/index.js
+++ crown-shop/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import  {store, persistor} from './redux/store';
 
-import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-          <App />
+          <PersistGate persistor={persistor}>   
+              <App />
+          </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
--- crown-shop/src/redux/root-reducer.js
+++ crown-shop/src/redux/root-reducer.js
@@ -1,10 +1,20 @@
 import { combineReducers } from 'redux';
-
+import {persistReducer} from 'redux-persist';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import storage from 'redux-persist/lib/storage';
+import directoryReducer from './directory/directory.reducer';
 
+const persistConfig={
+    key: 'root',
+    storage,
+    whitelist : ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
-    cart : cartReducer
-});
+    cart : cartReducer,
+    directory: directoryReducer,
+})
+
+export default persistReducer(persistConfig, rootReducer);
