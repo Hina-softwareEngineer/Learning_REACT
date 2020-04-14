3c5dd9820c7a59e74100ac09a011680e5add0acc
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-14 11:36:51 +0500

    data normalization

--- crown-shop/src/components/collections-overview/collections-overview.component.jsx
+++ crown-shop/src/components/collections-overview/collections-overview.component.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {selectCollections} from '../../redux/shop/shop.selector';
-import {createStructuredSelector} from 'reselect';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import CollectionPreview from '../preview-collection/preview-collection.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
+
 import './collections-overview.styles.scss';
 
-const CollectionsOverview =({collections})=>(
-    <div className='collections-overview'>
-        {
-            collections.map(({ id , ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps } />
-            ))
-        }
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-    </div>
-)
-
-const mapStateToProps= createStructuredSelector(
-    {
-        collections : selectCollections
-    }
-)
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+});
 
 export default connect(mapStateToProps)(CollectionsOverview);
