3ad89b4656e63bd3dacde6a132405b59c9880569
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-22 11:34:54 +0500

    completed backend stripe

--- crown-shop/client/src/components/stripe-button/stripe-button.component.jsx
+++ crown-shop/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
-
+import axios from 'axios';
 const StripeCheckoutButton =({price})=>{
     const priceForStripe= price*100;
     const publishableKey='pk_test_1Wm07C2Hm9zmpv9vLDm6nVpo00cbEBooGF';
   
   const  onToken= token =>{
-        console.log(token);
-        alert("Payment Successful");
+      axios({
+          url : 'payment',
+          method : 'post',
+          data : {
+              amount : priceForStripe,
+              token,
+          }
+      }).then(response=>{
+          alert('Payment Successful')
+      }).catch(error=>{
+          console.log('Payment error', JSON.parse(error));
+          alert('There was an issue with your payment. Please sure you use the provided credit card.');
+      });
+
     }
 
 
