4f282c9986d5a9bfc3c4b92857025ff4467e41e2
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-24 11:45:36 +0500

    graphQl understanding

--- crown-shop/client/src/components/collections-overview/collections-overview.container.jsx
+++ crown-shop/client/src/components/collections-overview/collections-overview.container.jsx
@@ -16,3 +16,45 @@ const CollectionsOverviewContainer = compose(
 )(CollectionsOverview);
 
 export default CollectionsOverviewContainer;
+
+/*
+ For graphQl
+
+ import {Query} from 'react-apollo';
+ import { gql } from 'apollo-boost';
+
+ import CollectionsOverview from './collections-overview.component';
+ import Spinner from '../spinner/spinner.component';
+
+ const GET_COLLECTIONS = gql`
+  {
+    collections{
+      id 
+      title
+      items{
+        id 
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+ 
+ `;
+
+ const CollectionsOverviewContainer = () => (
+   <Query query={GET_COLLECTIONS}>
+      {
+        ({ loading, error,data }) => {
+          console.log({loading, error, data});
+
+          if (loading) return <Spinner />;
+          return <CollectionOverview collections={data.collections} />
+        }
+      }
+   </Query>
+ );
+
+
+ export default CollectionsOverviewContainer;
+*/
--- crown-shop/client/src/index.js
+++ crown-shop/client/src/index.js
@@ -6,9 +6,45 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import  {store, persistor} from './redux/store';
+import {ApolloProvider} from 'react-apollo';
+import { createHttpLink} from 'apollo-link-http';
+import { InMemoryCache} from 'apollo-cache-inmemory';
+import { ApolloClient , gql} from 'apollo-boost';
+
+
+
+const httpLink = createHttpLink({
+  uri : 'https://crwn-clothing.com'
+});
+
+const cache=new InMemoryCache();
+
+const client= new ApolloClient({
+  link : httpLink,
+  cache
+});
+
+client.query({
+  query : gql`
+  {
+    getCollectionsByTitle(title : "hats"){
+      id 
+      title
+      items{
+        id 
+        name
+        price
+        imageUrls
+      }
+    }
+  }
+  `
+}).then(response => console.log(response))
+
 
 
 ReactDOM.render(
+  <ApolloProvider>
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
@@ -17,7 +53,8 @@ ReactDOM.render(
           </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
-  </Provider>,
+  </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
