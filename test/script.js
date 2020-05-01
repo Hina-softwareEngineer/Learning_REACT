8f0cdb99c972b109716ca9420f0266cf3f96e97c
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-01 16:40:44 +0500

    Revert "Revert "learn testing""
    This reverts commit 399d0528261778eb0b7bc1d5862921d60fab17c0.

---
+++ test/script.js
@@ -0,0 +1,18 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavouritecats.com'
+]
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
+        return website.includes(searchInput);
+    });
+
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+
+console.log(googleSearch('cats'));
