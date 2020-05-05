1336db8bf57a15f6f11b75ea7d33ed6932bda8bb
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-05 11:43:28 +0500

    issue in /markdown/

--- gatsby-blogger/gatsby-node.js
+++ gatsby-blogger/gatsby-node.js
@@ -5,3 +5,47 @@
  */
 
 // You can delete this file if you're not using it
+const path= require('path');
+const {createFilePath} = require('gatsby-source-filesystem');
+
+exports.onCreateNode = ({ node,getNode,  actions })=> {
+    const {createNodeField} = actions;
+    console.log(node.internal.type)
+    if(node.internal.type === 'MarkdownRemark'){
+        const slug = createFilePath({ node, getNode })
+        
+        createNodeField({
+            node, 
+            name: `slug`,
+            value: slug
+        })
+    }
+}
+
+exports.creatPages = ({graphql, actions}) => {
+    const { creatPages } = actions
+    return graphql(`
+    {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+      
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({node}) => {
+            createPage({
+                path : node.fields.slug,
+                component : path.resolve(`./src/templates/blog-post.js`),
+                context : {
+                    slug : node.fields.slug
+                },
+            })
+        })
+    })
+}
--- gatsby-blogger/src/pages/index.js
+++ gatsby-blogger/src/pages/index.js
@@ -1,13 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql,Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export default ({data}) => (
   <Layout>
     <SEO title="Home" />
+    <div>
+      <h1>Hina's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node})=>(
+          <div key={node.id}>
+            <h2>{node.frontmatter.title} - {node.frontmatter.date}</h2>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
+    </div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -18,4 +30,27 @@ const IndexPage = () => (
   </Layout>
 )
 
-export default IndexPage
+
+export const query = graphql`
+  query
+    {
+      allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+        totalCount
+        edges {
+          node {
+            id
+            frontmatter {
+              date
+              description
+              title
+            }
+            fields{
+              slug
+            }
+            excerpt
+          }
+        }
+      }
+    }
+   
+`
